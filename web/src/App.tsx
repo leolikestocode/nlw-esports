b8fd3c3717ce993acea88c98ae4a7662356924f8
leolikestocode <leonardofpereira25@gmail.com>
2022-09-28 13:30:37 -0300

    Home visual

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,52 @@
+import { MagnifyingGlassPlus } from "phosphor-react"
+
+import "./styles/main.css"
+import logoImg from "./assets/logo-nlw-esports.png"
+
 function App() {
-  return <h1>aa</h1>
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="logo" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{" "}
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        {[...new Array(6)].map((_, i) => (
+          <a className="relative rounded-l overflow-hidden">
+            <img src={`/game-${i + 1}.png`} alt="" />
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+              <strong className="font-bold text-white block">
+                League of Legends
+              </strong>
+              <span className="text-zinc-300 text-sm block">4 anúncios</span>
+            </div>
+          </a>
+        ))}
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+        <div className="bg-[#2a2634] px-8 py-6 flex justify-between">
+          <div className="">
+            <strong className="text-2xl text-white font-black block">
+              Não encontrou seu duo?
+            </strong>
+            <span className="text-zinc-400 block">
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+          <button className="py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-700 flex items-center gap-3">
+            <MagnifyingGlassPlus />
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
